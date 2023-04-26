1251e5946495bcda98946be5d1eced98104f6e8b
Mergen Allamyradov <ma.mergen@gmail.com>
2023-04-27 00:28:20 +0500

    v3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         //V1
         System.out.println("Java is easy");
         //V2
+        System.out.println("Don't forget to put dot while traveling between versions");
+        //V3
 
     }
 }
